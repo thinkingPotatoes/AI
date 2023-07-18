b50c180e954bf5ac6a046c8054a81110accded42
ahyeon0508 <ahyeon_0508@naver.com>
2023-07-19 01:26:37 +0900

    ✨ 나와 비슷한 취향을 가진 유저가 좋아한 영화 추천 API 개발 (#4)

---
+++ ai_api/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from AI.ai_core.sim_movies import recommendMovies
+
+app=Flask(__name__)
+
+@app.route("/recommend/<userID>", methods=['POST'])
+def recommendSimMovies(userID):
+    req = request.get_json()
+
+    userId = req['userId']
+    topN = req['topN']
+
+    results = recommendMovies(userId, topN)
+
+    result = {"userId": userId, "results": results}
+    return result
+
+if __name__ == '__main__':
+    app.run(host="localhost", port="9000", debug=True)
