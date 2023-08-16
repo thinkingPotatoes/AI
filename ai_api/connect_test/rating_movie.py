ba233b92a019d44d4feeb949165e7e43f43f7259
ahyeon0508 <ahyeon_0508@naver.com>
2023-08-16 20:34:37 +0900

    ✅ 영화 평점 예측 API test 변경 (#8)

--- ai_api/connect_test/rating_movie.py
+++ ai_api/connect_test/rating_movie.py
@@ -4,8 +4,8 @@ import requests
 url = "http://localhost:9000/rating/302"
 
 payload = json.dumps({
-    "userId": 196,
-    "movieId": 302
+    "userId": 302,
+    "movieId": ["A06376", "B00013"]
 })
 
 headers = {
