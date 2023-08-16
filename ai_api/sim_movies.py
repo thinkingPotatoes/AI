5f998fae6135f0e24cb617629ae2ba67ae606430
ahyeon0508 <ahyeon_0508@naver.com>
2023-08-17 00:43:31 +0900

    ✨ 나와 비슷한 취향을 가진 유저가 좋아한 영화 추천 API 기능 추가 (#8)

--- ai_api/sim_movies.py
+++ ai_api/sim_movies.py
@@ -5,9 +5,12 @@ import random
 from sqlalchemy import create_engine
 import yaml
 
+import requests
+import json
+
 def connectDB():
     # Connection DB
-    with open('../api_config.yaml', encoding='UTF8') as f:
+    with open('Ai/api_config.yaml', encoding='UTF8') as f:
         config = yaml.safe_load(f)
 
     user = config['database']['user']
@@ -16,7 +19,7 @@ def connectDB():
     port = int(config['database']['port'])
     database = config['database']['database']
 
-    engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}', encoding='utf-8')
+    engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}:{port}/{database}', connect_args={'charset':'utf8'})
     conn = engine.connect()
 
     return conn
@@ -27,8 +30,6 @@ def recommendMovies(userId, topN):
     sim_user = pd.read_sql(sim_user_sql, conn, index_col='userId')
     ratings_sql = 'SELECT * FROM ratings'
     ratings = pd.read_sql(ratings_sql, conn)
-
-    print(sim_user)
 
     sim_seen_movie = []
 
@@ -43,9 +44,33 @@ def recommendMovies(userId, topN):
     sim_seen_movie = list(itertools.chain(*sim_seen_movie))
 
     user_seen_movie = list(ratings[ratings['userId'] == userId]['movieId'])
-    results = list(set(sim_seen_movie).difference(user_seen_movie))
+    movieId_results = list(set(sim_seen_movie).difference(user_seen_movie))
 
-    if len(results) > topN:
-        results = random.sample(results, topN)
+    if len(movieId_results) > topN:
+        movieId_results = random.sample(movieId_results, topN)
 
-    return results
+    # Extract Movie Info
+    payload = json.dumps({
+        "userId": userId,
+        "movieId": movieId_results
+    })
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.post("http://localhost:9000/rating/"+userId, headers=headers, data=payload)
+    req_result = json.loads(response.text)
+
+    result = {}
+    for movieId in movieId_results:
+        try:
+            title_sql = 'SELECT title FROM movie where doc_id = "' + movieId + '"'
+            posterUrl_sql = 'SELECT posterUrl FROM movie where doc_id = "' + movieId + '"'
+            title = pd.read_sql(title_sql, conn)['title'].values[0]
+            posterUrl = pd.read_sql(posterUrl_sql, conn)['posterUrl'].values[0]
+        except:
+            posterUrl = ""
+        result[movieId] = [req_result['result'][movieId], title, posterUrl]
+
+    return result
