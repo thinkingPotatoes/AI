aea261f240ed26ab72d896ba7032ad6296a0a8bd
ahyeon0508 <ahyeon_0508@naver.com>
2023-08-25 19:20:16 +0900

    💡 주석 작성 (#8)

--- ai_api/sim_movies.py
+++ ai_api/sim_movies.py
@@ -38,12 +38,15 @@ def recommendMovies(userId, topN):
 
     for i in sim_users:
         user_rating = ratings[ratings['userId'] == i]
+        # Utilize ratings distribution of users of similar tastes
         user_rating_Q3 = user_rating['rating'].quantile(.75)
+        # Extract movies rated higher than the value of Q3
         sim_seen_movie.append(list(user_rating[user_rating['rating'] >= user_rating_Q3].sort_values(ascending=False, by='rating')['movieId']))
 
     sim_seen_movie = list(itertools.chain(*sim_seen_movie))
 
     user_seen_movie = list(ratings[ratings['userId'] == userId]['movieId'])
+    # Extract only recommended movies that user hasn't watched
     movieId_results = list(set(sim_seen_movie).difference(user_seen_movie))
 
     # Extract Movie Info
@@ -67,6 +70,7 @@ def recommendMovies(userId, topN):
             title = pd.read_sql(title_sql, conn)['title'].values[0]
             posterUrl = pd.read_sql(posterUrl_sql, conn)['posterUrl'].values[0]
         except:
+            # Not exist posterUrl
             posterUrl = ""
         result[movieId] = [req_result['result'][movieId], title, posterUrl]
 
